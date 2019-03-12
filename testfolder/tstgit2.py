e4cfb2e5914870c5f53b2e9215d2158b450d61ff
livio <livio.giacomini@hotmail.com>
2019-03-12 10:30:26 +0100

    Commit from test2 branch

--- testfolder/tstgit2.py
+++ testfolder/tstgit2.py
@@ -5,3 +5,8 @@ print("Hello")
 print("test")
 
 print("test2")
+
+
+# Insert some code and then I try to start again without this code from a new branch
+
+# This comment is added in test2 branch (the one before in test1 branch)
